9b067d6cf142499c84ac7f6dff51d504582f4fb0
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-11-29 22:01:58 +0200

    Update userRouter.js

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNewAdmin, login, patientRegister, getAllDoctors} from "../controller/userController.js";
+import { addNewAdmin, login, patientRegister, getAllDoctors, getUserDetails, logoutAdmin, logoutPatient, addNewDoctor} from "../controller/userController.js";
 import { isAdminAuthenticated,
     isAPatientAuthenticated,
  } from "../middlewares/auth.js";
@@ -10,5 +10,11 @@ router.post("/patient/register", patientRegister);
 router.post("/login", login);
 router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
 router.get("/doctors", getAllDoctors);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+router.get("/patient/me", isAPatientAuthenticated, getUserDetails);
+router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+router.get("/patient/logout", isAPatientAuthenticated, logoutPatient);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
+
 
 export default router;

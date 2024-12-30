3cd3b815524c6f6f5d1e49a7432f77314490f209
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-30 23:47:38 +0200

    Update appointmentRouter.js

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { postAppointment } from "../controller/appointmentController.js";
-
+import { getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
+import { isAdminAuthenticated, isAPatientAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.post("/post", postAppointment);
+router.post("/post", isAPatientAuthenticated, postAppointment);
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
 
 export default router;

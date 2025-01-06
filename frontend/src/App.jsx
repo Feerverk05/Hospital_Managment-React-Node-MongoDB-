d817a3d034c76f4f7fe3eb2a3231b8b3695600d3
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-06 23:07:23 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Appointment from "./pages/Appointment";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AboutUs from "./pages/AboutUs";
-
+import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path = '/' element={<Home/>}/>
-          <Route path = '/appointment' element={<Appointment/>}/>
-          <Route path = '/login' element={<Login/>}/>
-          <Route path = '/register' element={<Register/>}/>
-          <Route path = '/aboutus' element={<AboutUs/>}/>
-        </Routes> 
-        <ToastContainer position="top-center"/>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <ToastContainer position="top-center" />
       </Router>
     </>
   )

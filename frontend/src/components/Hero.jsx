66d21814aeefab28615ce15f833dbb4ca87acdc1
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-06 22:26:30 +0200

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({title, imageUrl}) => {
   return (
-    <div>
-
+    <div className="hero container">
+      <div className="banner">
+        <h1>{title}</h1>
+        <p>
+          В жопу зробимо укольчик <br/>
+          Раз два три і ти в окопі
+        </p>
+      </div>
+      <div className="banner">
+        <img src={imageUrl} alt="hero" className="animated-image"/>
+        <span>
+          <img src="/Vector.png" alt="vector" />
+        </span>
+      </div>
     </div>
   )
 }

5c294e30f5182a606dbb9724bac21ce7444dcdfe
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-08 00:01:02 +0200

    Create AppointmentForm.jsx

---
+++ frontend/src/components/AppointmentForm.jsx
@@ -0,0 +1,217 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const AppointmentForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [nic, setNic] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [appointmentDate, setAppointmentDate] = useState("");
+  const [department, setDepartment] = useState("Pediatrics");
+  const [doctorFirstName, setDoctorFirstName] = useState("");
+  const [doctorLastName, setDoctorLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [hasVisited, setHasVisited] = useState(false);
+
+  const departmentsArray = [
+    "Педіатр",
+    "Ортопед",
+    "Кардіолог",
+    "Невролог",
+    "Онколог",
+    "МРТ",
+    "Фізична терапія",
+    "Дерматолог",
+    "ЛОР",
+  ];
+
+  const [doctors, setDoctors] = useState([]);
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/doctors",
+        { withCredentials: true }
+      );
+      setDoctors(data.doctors);
+      console.log(data.doctors);
+    };
+    fetchDoctors();
+  }, []);
+  const handleAppointment = async (e) => {
+    e.preventDefault();
+    try {
+      const hasVisitedBool = Boolean(hasVisited);
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/appointment/post",
+        {
+          firstName,
+          lastName,
+          email,
+          phone,
+          nic,
+          dob,
+          gender,
+          appointment_date: appointmentDate,
+          department,
+          doctor_firstName: doctorFirstName,
+          doctor_lastName: doctorLastName,
+          hasVisited: hasVisitedBool,
+          address,
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      toast.success(data.message);
+      setFirstName(""),
+        setLastName(""),
+        setEmail(""),
+        setPhone(""),
+        setNic(""),
+        setDob(""),
+        setGender(""),
+        setAppointmentDate(""),
+        setDepartment(""),
+        setDoctorFirstName(""),
+        setDoctorLastName(""),
+        setHasVisited(""),
+        setAddress("");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="container form-component appointment-form">
+        <h2>Записатися на прийом</h2>
+        <form onSubmit={handleAppointment}>
+          <div>
+            <input
+              type="text"
+              placeholder="Прізвище"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Ім'я"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Телефон"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Ідентифікаційний код"
+              value={nic}
+              onChange={(e) => setNic(e.target.value)}
+            />
+            <input
+              type="date"
+              placeholder="Дата народження"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+          </div>
+          <div>
+            <select value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="">Оберіть стать:</option>
+              <option value="Male">Чоловік</option>
+              <option value="Female">Жінка</option>
+            </select>
+            <input
+              type="date"
+              placeholder="Дата запису"
+              value={appointmentDate}
+              onChange={(e) => setAppointmentDate(e.target.value)}
+            />
+          </div>
+          <div>
+            <select
+              value={department}
+              onChange={(e) => {
+                setDepartment(e.target.value);
+                setDoctorFirstName("");
+                setDoctorLastName("");
+              }}
+            >
+              {departmentsArray.map((depart, index) => {
+                return (
+                  <option value={depart} key={index}>
+                    {depart}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              value={`${doctorFirstName} ${doctorLastName}`}
+              onChange={(e) => {
+                const [firstName, lastName] = e.target.value.split(" ");
+                setDoctorFirstName(firstName);
+                setDoctorLastName(lastName);
+              }}
+              disabled={!department}
+            >
+              <option value="">Оберіть лікаря</option>
+              {doctors
+                .filter((doctor) => doctor.doctorDepartment === department)
+                .map((doctor, index) => (
+                  <option
+                    value={`${doctor.firstName} ${doctor.lastName}`}
+                    key={index}
+                  >
+                    {doctor.firstName} {doctor.lastName}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <textarea
+            rows="10"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Причина візиту"
+          />
+          <div
+            style={{
+              gap: "10px",
+              justifyContent: "flex-end",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ marginBottom: 0 }}>Вже маєте направлення?</p>
+            <input
+              type="checkbox"
+              checked={hasVisited}
+              onChange={(e) => setHasVisited(e.target.checked)}
+              style={{ flex: "none", width: "25px" }}
+            />
+          </div>
+          <button style={{ margin: "0 auto" }}>Отримати направлення</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AppointmentForm;

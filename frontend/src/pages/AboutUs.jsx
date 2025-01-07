deaf1faeb2a463b2bcefcfd70f80aecb644a445b
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-08 00:26:17 +0200

    Update AboutUs.jsx

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
 const AboutUs = () => {
   return (
-    <div>
-
-    </div>
+    <>
+    <Hero title={"Health"} imageUrl={"/t.png"}/>
+    <Biography imageUrl={"/d.png"} width="auto" height="200px"/>
+    </>
   )
 }
 

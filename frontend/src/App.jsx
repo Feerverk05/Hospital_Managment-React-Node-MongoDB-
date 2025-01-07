8c36ed0931dbfd7399113b844c650eafde32db4a
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-07 16:56:35 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,8 +8,25 @@ import Register from "./pages/Register";
 import AboutUs from "./pages/AboutUs";
 import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { Context } from "./main";
+import axios from "axios";
+
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+  useEffect(()=>{
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/api/v1/user/patient/me", {withCredentials: true });
+        setIsAuthenticated(true);
+        setUser(response.sata.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
   return (
     <>
       <Router>

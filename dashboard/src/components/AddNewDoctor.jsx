05f9a3970f4dd2d6c0e6e7fb5484d5dbf8f7de0c
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-10 00:21:32 +0200

    Update AddNewDoctor.jsx

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -1,11 +1,171 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom';
+import { Context } from '../main';
 
 const AddNewDoctor = () => 
 {
-    return (
-        <div>
+    const { isAuthenticated, setIsAuthenticated } = useContext(Context);
 
-        </div>
-    )
-}
-export default AddNewDoctor
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [nic, setNic] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+  const [doctorDepartment, setDoctorDepartment] = useState("");
+  const [docAvatar, setDocAvatar] = useState("");
+  const [docAvatarPreview, setDocAvatarPreview] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const departmentsArray = [
+    "Педіатр",
+    "Ортопед",
+    "Кардіолог",
+    "Невролог",
+    "Онколог",
+    "МРТ",
+    "Фізична терапія",
+    "Дерматолог",
+    "ЛОР",
+  ];
+
+  const handleAvatar = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        setDocAvatarPreview(reader.result);
+        setDocAvatar(file);
+    };
+  };
+
+  const handleAddNewDoctor = async (e) => {
+    e.preventDefault();
+    try {
+        const formData = new FormData();
+      formData.append("firstName", firstName);
+      formData.append("lastName", lastName);
+      formData.append("email", email);
+      formData.append("phone", phone);
+      formData.append("password", password);
+      formData.append("nic", nic);
+      formData.append("dob", dob);
+      formData.append("gender", gender);
+      formData.append("doctorDepartment", doctorDepartment);
+      formData.append("docAvatar", docAvatar);
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/user/doctor/addnew", formData,
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "multipart/form-data" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhone("");
+          setNic("");
+          setDob("");
+          setGender("");
+          setPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+    return (
+        <section className='page'>
+            <section className='container add-doctor-form'>
+                <img src="/logo.png" alt="logo" className='logo'/>
+                <h1 className='form-title'>Додати нового доктора</h1>
+                <form onSubmit={handleAddNewDoctor}>
+          <div className='first-wrapper'>
+            <div>
+                <img src={ docAvatarPreview ? `${docAvatarPreview}` : "/doc.jpg"} alt="Doctor Avatar"/>
+                <input type="file" onChange={handleAvatar}/>
+                </div>
+                <div>
+            <input
+              type="text"
+              placeholder="Ім'я"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Прізвище"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Телефон"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Індентифікаційний код"
+              value={nic}
+              onChange={(e) => setNic(e.target.value)}
+            />
+            <input
+              type={"date"}
+              placeholder="Дата народження"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+            <select value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="">Оберіть стать:</option>
+              <option value="Male">Жінка</option>
+              <option value="Female">Чоловік</option>
+            </select>
+            <input   
+              type="password"
+              placeholder="Пароль"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <select
+                value={doctorDepartment}
+                onChange={(e) => {
+                  setDoctorDepartment(e.target.value);
+                }}
+              >
+                <option value="">Select Department</option>
+                {departmentsArray.map((depart, index) => {
+                  return (
+                    <option value={depart} key={index}>
+                      {depart}
+                    </option>
+                  );
+                })}
+              </select>
+            <button type="submit">Додати нового лікаря</button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </section>
+  );
+};
+
+export default AddNewDoctor;

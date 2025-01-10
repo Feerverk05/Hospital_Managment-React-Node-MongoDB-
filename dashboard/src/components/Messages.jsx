275c0f72aa69f2b31662446d603d117e3abc20e1
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-10 19:06:00 +0200

    Update Messages.jsx

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -1,55 +1,65 @@
-import React, { useContext, useEffect, useState } from 'react';
 import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../main";
 import { Navigate } from "react-router-dom";
-import { Context } from '../main';
 
-const Messages = () => 
-{
-    const [messages, setMessages] = useState("");
-    const { isAuthenticated, setIsAuthenticated } = useContext(Context);
-    
-    useEffect(() => {
-        const fetchMessages = async () => {
-            try {
-                const { data } = await axios.get (
-                    "http://localhost:4000/api/v1/message/getall",
-                    { withCredentials: true}
-                );
-                setMessages(data.messages);
-            } catch (error) {
-                console.log( "Праблемс", error);
-            }
-        };
-        fetchMessages();
-    }, []);
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/message/getall",
+          { withCredentials: true }
+        );
+        setMessages(data.messages);
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    };
+    fetchMessages();
+  }, []);
 
-    if (isAuthenticated) {
-        return <Navigate to={"/login"} />;
-      } 
-    return (
-        <section className='page messages'>
-            <h1>Повідомлення</h1>
-            <div className='banner'>
-                {messages && messages.length > 0 ? (
-                    messages.map((element) => {
-                        return (
-                            <div className='card' key={element._id}>
-                                <div className='details'>
-                                    <p>Прізвище: <span>{element.firstName}</span></p>
-                                    <p>Ім'я: <span>{element.lastName}</span></p>
-                                    <p>Email: <span>{element.email}</span></p>
-                                    <p>Телефон: <span>{element.phone}</span></p>
-                                    <p>Повідомлення: <span>{element.message}</span></p>
-                                </div>
-                                </div>
-                        );
-                    })
-                ) : (
-                    <h1>Немає повідомлень</h1>
-                )}
-            </div>
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
 
-        </section>
-    );
+  return (
+    <section className="page messages">
+      <h1>Повідомлення клієнтів</h1>
+      <div className="banner">
+        {messages && messages.length > 0 ? (
+          messages.map((element) => {
+            return (
+              <div className="card" key={element._id}>
+                <div className="details">
+                  <p>
+                    Ім'я: <span>{element.firstName}</span>
+                  </p>
+                  <p>
+                   Прізвище: <span>{element.lastName}</span>
+                  </p>
+                  <p>
+                    Email: <span>{element.email}</span>
+                  </p>
+                  <p>
+                    Телефон: <span>{element.phone}</span>
+                  </p>
+                  <p>
+                    Повідомлення: <span>{element.message}</span>
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>Повідомлення!</h1>
+        )}
+      </div>
+    </section>
+  );
 };
-export default Messages
+
+export default Messages;
